8a771cbfbaeb410d4f1e290262a5e38d75f91479
Gustavo Miyamoto <glmiyamoto@gmail.com>
2018-12-26 17:54:09 -0200

    Add project base structure and resources

---
+++ MountainChallenge/Model/Mountain.swift
@@ -0,0 +1,22 @@
+//
+//  Mountain.swift
+//  MountainChallenge
+//
+//  Created by Gustavo Miyamoto on 26/12/18.
+//  Copyright © 2018 Gustavo Miyamoto. All rights reserved.
+//
+
+import Foundation
+
+struct Mountain {
+    var id: Int
+    var areaId: Int
+    var name: String
+    var elevation: Double
+    var description: String
+    var prefectures: [String]
+    var likeCount: Int
+    var isLike: Bool
+    var thumbnailUrl: String
+    var imageUrl: String
+}
--- Podfile
+++ Podfile
@@ -3,7 +3,6 @@ platform :ios, '10.0'
 use_frameworks!
 
 target 'MountainChallenge' do
-    inherit! :search_paths
-
     pod 'Alamofire', '~> 4.7'
+    pod 'AlamofireImage', '~> 3.5'
 end
