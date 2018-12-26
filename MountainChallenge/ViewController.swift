363324ecae74a24f2ab76bee1a6baf04861acde6
Gustavo Miyamoto <glmiyamoto@gmail.com>
2018-12-26 16:18:24 -0200

    Add base project

---
+++ MountainChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MountainChallenge
+//
+//  Created by Gustavo Miyamoto on 26/12/18.
+//  Copyright © 2018 Gustavo Miyamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
