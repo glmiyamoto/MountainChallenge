19f393cffc7005a2e4ca12a70543776a1a5aa0d5
Gustavo Miyamoto <glmiyamoto@gmail.com>
2018-12-28 20:42:46 -0200

    Implemented UI test

--- MountainChallengeUITests/MountainChallengeUITests.swift
+++ MountainChallengeUITests/MountainChallengeUITests.swift
@@ -9,26 +9,67 @@
 import XCTest
 
 class MountainChallengeUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testBaseNavigationWithoutLikeSelection() {
+        // Accessing mountain list
+        let table = app.tables.firstMatch
+        
+        // Select a first one
+        let firstCell = table.cells.firstMatch
+        XCTAssertTrue(firstCell.exists)
+        firstCell.tap()
+        
+        // Acessing mountain detail
+        let likeCountLabel = app.staticTexts["LikeCountLabel"]
+        XCTAssertEqual(likeCountLabel.label, "175")
+        
+        // Select to back
+        app.navigationBars.buttons.firstMatch.tap()
+        
+        // Assert first cell like count
+        XCTAssertTrue(firstCell.exists)
+        let tableCellLikeCountLabel = firstCell.staticTexts.element(boundBy: 2)
+        XCTAssertEqual(tableCellLikeCountLabel.label, "175")
+    }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testBaseNavigationWithLikeSelection() {
+        // Accessing mountain list
+        let table = app.tables.firstMatch
+        
+        // Select a first one
+        let firstCell = table.cells.firstMatch
+        XCTAssertTrue(firstCell.exists)
+        firstCell.tap()
+        
+        // Acessing mountain detail
+        let likeCountLabel = app.staticTexts["LikeCountLabel"]
+        XCTAssertEqual(likeCountLabel.label, "175")
+        
+        // Select a like button
+        app.buttons["LikeButton"].tap()
+        
+        // Assert mountain detail like count
+        XCTAssertEqual(likeCountLabel.label, "176")
+        
+        // Select to back
+        app.navigationBars.buttons.firstMatch.tap()
+        
+        // Assert first cell like count
+        XCTAssertTrue(firstCell.exists)
+        let tableCellLikeCountLabel = firstCell.staticTexts.element(boundBy: 2)
+        XCTAssertEqual(tableCellLikeCountLabel.label, "176")
     }
 
 }
